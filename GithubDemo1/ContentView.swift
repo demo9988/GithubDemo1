5171d9e05ebe57ee1c7b326d14648ede6aa47ed9
Mousa Sweidan <msweidan@hct.ac.ae>
2023-08-08 12:35:06 +0300

    Initial Commit

---
+++ GithubDemo1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubDemo1
+//
+//  Created by msweidan on 08/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubDemo1/GithubDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemo1App.swift
+//  GithubDemo1
+//
+//  Created by msweidan on 08/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
