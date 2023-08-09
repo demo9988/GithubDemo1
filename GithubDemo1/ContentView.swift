aafaeab76041205b1907619d026d7dbc265dd2b9
Mousa Sweidan <msweidan@hct.ac.ae>
2023-08-09 10:28:52 +0300

    Add a checkmark

--- GithubDemo1/ContentView.swift
+++ GithubDemo1/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
+        Image(systemName: "checkmark")
+                .font(.system(size: 40))
         Text("Welcome to GitHub")
             .padding()
             .font(.title)
